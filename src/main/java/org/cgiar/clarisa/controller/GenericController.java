73f446614da880d4b0a345d46028ace66157c206
GMartinez <GMartinez@CIATCO00529L.CGIARAD.ORG>
2022-02-15 10:02:49 -0500

    Refactoring GenericController
    - Changing signature from interface to abstract class
    - Simplifying find by ID logic
    - Adding advice to catch the new exception when an entity is not found

--- src/main/java/org/cgiar/clarisa/controller/GenericController.java
+++ src/main/java/org/cgiar/clarisa/controller/GenericController.java
@@ -1,26 +1,28 @@
 /*****************************************************************
- * This file is part of Managing Agricultural Research for Learning &
- * Outcomes Platform (MARLO).
- * MARLO is free software: you can redistribute it and/or modify
+ * This file is part of CGIAR Level Agricultural Results
+ * Interoperable System Architecture (CLARISA).
+ * CLARISA is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * at your option) any later version.
- * MARLO is distributed in the hope that it will be useful,
+ * CLARISA is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License
- * along with MARLO. If not, see <http://www.gnu.org/licenses/>.
+ * along with CLARISA. If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************/
 
 package org.cgiar.clarisa.controller;
 
 import org.cgiar.clarisa.dto.BaseDTO;
+import org.cgiar.clarisa.exception.EntityNotFoundException;
 import org.cgiar.clarisa.manager.GenericManager;
 import org.cgiar.clarisa.mapper.BaseMapper;
 import org.cgiar.clarisa.model.ClarisaBaseEntity;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -42,72 +44,71 @@ import org.springframework.web.bind.annotation.RequestBody;
  * @author German C. Martinez - CIAT/CCAFS
  **************/
 
-public interface GenericController<ENTITY extends ClarisaBaseEntity, DTO extends BaseDTO> {
+public abstract class GenericController<ENTITY extends ClarisaBaseEntity, DTO extends BaseDTO> {
 
-  public default DTO applyPatchToDTO(JsonPatch patch, DTO dto) throws JsonPatchException, JsonProcessingException {
+  private Class<ENTITY> entityClass;
+
+  protected GenericController(Class<ENTITY> entityClass) {
+    this.entityClass = Objects.requireNonNull(entityClass);
+  }
+
+  public DTO applyPatchToDTO(JsonPatch patch, DTO dto) throws JsonPatchException, JsonProcessingException {
     JsonNode patched = patch.apply(this.getObjectMapper().convertValue(dto, JsonNode.class));
     BaseDTO result = this.getObjectMapper().treeToValue(patched, dto.getClass());
     return (DTO) result;
   }
 
-  public default ENTITY applyPatchToEntity(JsonPatch patch, ENTITY entity)
-    throws JsonPatchException, JsonProcessingException {
+  public ENTITY applyPatchToEntity(JsonPatch patch, ENTITY entity) throws JsonPatchException, JsonProcessingException {
     JsonNode patched = patch.apply(this.getObjectMapper().convertValue(entity, JsonNode.class));
     ClarisaBaseEntity result = this.getObjectMapper().treeToValue(patched, entity.getClass());
     return (ENTITY) result;
   }
 
   @GetMapping(value = "/count")
-  public default ResponseEntity<Long> count() {
+  public ResponseEntity<Long> count() {
     return ResponseEntity.ok(this.getManager().count());
   }
 
   @DeleteMapping(value = "/delete")
-  public default void delete(@RequestBody DTO dto) {
+  public void delete(@RequestBody DTO dto) {
     ENTITY entity = this.getMapper().dtoToEntity(dto);
     this.getManager().delete(entity);
   }
 
   @DeleteMapping(value = "/delete/{id}")
-  public default ResponseEntity<Boolean> deleteById(@PathVariable("id") Long id) {
+  public ResponseEntity<Boolean> deleteById(@PathVariable("id") Long id) {
     this.getManager().deleteById(id);
     return ResponseEntity.ok(!this.getManager().existsById(id));
   }
 
   @GetMapping(value = "/exists/{id}")
-  public default ResponseEntity<Boolean> existsById(@PathVariable("id") Long id) {
+  public ResponseEntity<Boolean> existsById(@PathVariable("id") Long id) {
     Boolean exists = this.getManager().existsById(id);
     return ResponseEntity.ok(exists);
   }
 
-  @GetMapping(value = "/")
-  public default ResponseEntity<List<DTO>> findAll() {
+  @GetMapping(value = "/all")
+  public ResponseEntity<List<DTO>> findAll() {
     List<ENTITY> entitys = this.getManager().findAll();
     return ResponseEntity.ok(this.getMapper().entityListToDtoList(entitys));
   }
 
   @GetMapping(value = "/get/{id}")
-  public default ResponseEntity<DTO> findById(@PathVariable("id") Long id) {
-    Optional<ENTITY> entity = this.getManager().findById(id);
-
-    if (entity.isPresent()) {
-      return ResponseEntity.ok(this.getMapper().entityToDto(entity.get()));
-    } else {
-      this.getClassLogger().info("No entity found with id {}", id);
-      return ResponseEntity.notFound().build();
-    }
+  public ResponseEntity<DTO> findById(@PathVariable("id") Long id) {
+    ENTITY entity = this.getManager().findById(id).orElseThrow(() -> new EntityNotFoundException(this.entityClass, id));
+    return ResponseEntity.ok(this.getMapper().entityToDto(entity));
   }
 
-  public Logger getClassLogger();
+  public abstract Logger getClassLogger();
 
-  public GenericManager<ENTITY> getManager();
+  public abstract GenericManager<ENTITY> getManager();
 
-  public BaseMapper<ENTITY, DTO> getMapper();
+  public abstract BaseMapper<ENTITY, DTO> getMapper();
 
-  public ObjectMapper getObjectMapper();
+  public abstract ObjectMapper getObjectMapper();
 
   @PatchMapping(value = "/patch/{id}")
-  public default ResponseEntity<DTO> patch(@PathVariable("id") Long id, @RequestBody JsonPatch patch) {
+  public ResponseEntity<DTO> patch(@PathVariable("id") Long id, @RequestBody JsonPatch patch) {
     Optional<ENTITY> entityOptional = this.getManager().findById(id);
     if (entityOptional.isPresent()) {
       try {
@@ -124,14 +125,14 @@ public interface GenericController<ENTITY extends ClarisaBaseEntity, DTO extends
   }
 
   @PostMapping(value = "/save")
-  public default ResponseEntity<DTO> save(@RequestBody DTO dto) {
+  public ResponseEntity<DTO> save(@RequestBody DTO dto) {
     ENTITY entity = this.getMapper().dtoToEntity(dto);
     entity = this.getManager().save(entity);
     return ResponseEntity.ok(this.getMapper().entityToDto(entity));
   }
 
   @PutMapping(value = "/update")
-  public default ResponseEntity<DTO> update(@RequestBody DTO dto) {
+  public ResponseEntity<DTO> update(@RequestBody DTO dto) {
     ENTITY entity = this.getMapper().dtoToEntity(dto);
     entity = this.getManager().update(entity);
     return ResponseEntity.ok(this.getMapper().entityToDto(entity));
