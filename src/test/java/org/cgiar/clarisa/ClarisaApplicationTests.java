c5bd282c00e9cbe8fd3e71906d5ca6b1c152cf8f
Diego Perez <yeyo2k3@gmail.com>
2022-03-31 15:06:33 -0500

    CLARISA Junit test

--- src/test/java/org/cgiar/clarisa/ClarisaApplicationTests.java
+++ src/test/java/org/cgiar/clarisa/ClarisaApplicationTests.java
@@ -1,13 +1,36 @@
 package org.cgiar.clarisa;
 
+import org.cgiar.clarisa.manager.UserManager;
+import org.cgiar.clarisa.model.User;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@TestPropertySource({"classpath:config/clarisa-${spring.profiles.active:local}.properties"})
 class ClarisaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
+  @Autowired
+  UserManager userManager;
+
+
+  @Test
+  void contextLoads() {
+    System.out.println("context Load");
+  }
+
+
+  @Test
+  public void findUser() {
+    User user = userManager.findById(1082L).orElse(null);
+    System.out.println("Testing----- user Manager");
+    assertTrue(user != null);
+  }
+
 
 }
