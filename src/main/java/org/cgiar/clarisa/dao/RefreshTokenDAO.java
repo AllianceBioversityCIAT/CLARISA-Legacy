5213b1c63ce6e5fb23f2aad6189b9b71fec22638
German Martinez <g.martinez@cgiar.org>
2022-04-29 11:13:47 -0500

    Adding refresh token: migration, entity, dao and managers

---
+++ src/main/java/org/cgiar/clarisa/dao/RefreshTokenDAO.java
@@ -0,0 +1,32 @@
+/*****************************************************************
+ * This file is part of CGIAR Level Agricultural Results
+ * Interoperable System Architecture (CLARISA).
+ * CLARISA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * CLARISA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with CLARISA. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+package org.cgiar.clarisa.dao;
+
+import org.cgiar.clarisa.model.RefreshToken;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**************
+ * @author German C. Martinez - Alliance Bioversity/CIAT
+ **************/
+
+public interface RefreshTokenDAO extends JpaRepository<RefreshToken, Long> {
+
+  public Optional<RefreshToken> findFromToken(String token);
+
+}
