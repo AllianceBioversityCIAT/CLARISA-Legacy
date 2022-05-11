edf627846145c91cfc8d82902c4467511e0f75cc
German Martinez <g.martinez@cgiar.org>
2022-05-11 11:23:06 -0500

    Adding AppConstants

---
+++ src/main/java/org/cgiar/clarisa/utils/AppConstants.java
@@ -0,0 +1,26 @@
+/*****************************************************************
+ * This file is part of CGIAR Level Agricultural Results
+ * Interoperable System Architecture (CLARISA).
+ * CLARISA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * CLARISA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with CLARISA. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+package org.cgiar.clarisa.utils;
+
+
+/**************
+ * @author German C. Martinez - Alliance Bioversity/CIAT
+ **************/
+
+public class AppConstants {
+
+  public static final String HTTP_ENTITY_CLASS_NAME = "EntityClass";
+}
