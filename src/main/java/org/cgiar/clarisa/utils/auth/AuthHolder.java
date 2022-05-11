fbe0b982f00f68d59af83b9b458d572a52a77151
German Martinez <g.martinez@cgiar.org>
2022-05-11 11:20:19 -0500

    (#34) Authorization (logged user) holder

---
+++ src/main/java/org/cgiar/clarisa/utils/auth/AuthHolder.java
@@ -0,0 +1,27 @@
+/*****************************************************************
+ * This file is part of CGIAR Level Agricultural Results
+ * Interoperable System Architecture (CLARISA).
+ * CLARISA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * CLARISA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with CLARISA. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+package org.cgiar.clarisa.utils.auth;
+
+import org.cgiar.clarisa.model.User;
+
+/**************
+ * @author German C. Martinez - Alliance Bioversity/CIAT
+ **************/
+
+public interface AuthHolder {
+
+  User getCurrentUser();
+}
---
+++ src/main/java/org/cgiar/clarisa/utils/auth/AuthHolderImpl.java
@@ -0,0 +1,34 @@
+/*****************************************************************
+ * This file is part of CGIAR Level Agricultural Results
+ * Interoperable System Architecture (CLARISA).
+ * CLARISA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * CLARISA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with CLARISA. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+package org.cgiar.clarisa.utils.auth;
+
+import org.cgiar.clarisa.model.User;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+/**************
+ * @author German C. Martinez - Alliance Bioversity/CIAT
+ **************/
+
+@Component
+public class AuthHolderImpl implements AuthHolder {
+
+  @Override
+  public User getCurrentUser() {
+    return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+  }
+}
